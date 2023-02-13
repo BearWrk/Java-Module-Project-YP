4ea944ad28d4944ff9898e97623c6a1b1204d320
BearWrk <124399331+BearWrk@users.noreply.github.com>
2023-02-13 03:47:22 +0300

    Revert "my calculator"

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,20 +1,8 @@
-// dev branch for Y.Practicum
 public class Main {
 
     public static void main(String[] args) {
         // ваш код начнется здесь
         // вы не должны ограничиваться только классом Main и можете создавать свои классы по необходимости
-
-        Count guestCount =  new Count();
-        Calculator check = new Calculator();
-        check.newTovar();
-        check.print();
-
-        double checkPerOne = check.getSum()/guestCount.count();
-        RubFormat rub = new RubFormat();
-        System.out.println("Вас было "+guestCount+", значит с каждого по " + String.format("%.2f", checkPerOne) + rub.format(checkPerOne));
-
+        System.out.println("Привет Мир");
     }
-    }
-
-
+}
